0d97eefa61f661c1bc2043997b51db3938a6f472
Léonard Commelin <leo.commelin@gmail.com>
2022-10-08 12:58:42 +0200

    should fail

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -7,7 +7,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(): strin {
     return this.appService.getHello();
   }
 }
