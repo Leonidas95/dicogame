30311e52e12186d3675dc0dcef572a9de0d2359b
Léonard Commelin <leo.commelin@gmail.com>
2022-10-23 17:38:25 +0200

    update seed to add random words

--- apps/api/prisma/seed.ts
+++ apps/api/prisma/seed.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Word, WordDefinition } from '@prisma/client';
+import { faker } from '@faker-js/faker';
 
 const prisma = new PrismaClient();
 
@@ -13,6 +14,52 @@ const main = async () => {
     where: { username: 'admin' },
     update: {},
   });
+
+  await addWords();
+};
+
+const addWords = async () => {
+  await prisma.wordDefinition.deleteMany({}); // use with caution.
+  await prisma.word.deleteMany({}); // use with caution.
+
+  const languages = await prisma.language.findMany();
+
+  const WORDS_TO_ADD = 10;
+  const words: Word[] = [];
+  const wordDefinitions: WordDefinition[] = [];
+
+  languages.forEach((language) => {
+    faker.locale = language.iso.slice(0, 2);
+    for (let i = 0; i < WORDS_TO_ADD; ++i) {
+      const wordId = faker.datatype.uuid();
+      const date = faker.date.recent();
+      words.push({
+        id: wordId,
+        name: faker.word.noun().toUpperCase(),
+        languageId: language.id,
+        createdAt: date,
+        updatedAt: date,
+      });
+
+      const numberOfDefinitions = randomNumber(1, 3);
+      for (let j = 0; j < numberOfDefinitions; j++) {
+        wordDefinitions.push({
+          id: faker.datatype.uuid(),
+          definition: faker.lorem.sentence(),
+          wordId,
+          createdAt: date,
+          updatedAt: date,
+        });
+      }
+    }
+  });
+
+  await prisma.word.createMany({ data: words });
+  await prisma.wordDefinition.createMany({ data: wordDefinitions });
+};
+
+const randomNumber = (min: number, max: number): number => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 main()
