9e1298ae410045c5ccfc1119d5ada8dfe9d5652c
Léonard Commelin <leo.commelin@gmail.com>
2022-11-05 11:21:11 +0100

    tailwind (#13)

---
+++ packages/config/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "../../packages/ui/**/*.{ts,tsx}",
+    "./index.html",
+    "./src/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
