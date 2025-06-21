ea95031f515e1d2d624f1a005f3c9d6d85359d7a
Leonard Commelin <leo.commelin@gmail.com>
2025-06-21 12:16:51 +0200

    update Vite configuration for production base path and modify CI workflow to download build artifacts instead of restoring npm cache.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+	base: process.env.NODE_ENV === 'production' ? '/dicogame/' : '/',
 	plugins: [react(), tailwindcss()],
 	resolve: {
 		alias: {
